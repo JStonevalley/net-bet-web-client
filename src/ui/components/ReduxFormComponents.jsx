a32822d6364e4f4ed42ccb16a39406d512d6df7c
Jonas Stendahl <jonas.stendahl@outlook.com>
2018-04-01 16:31:37 +0100

    lint fix

--- src/ui/components/ReduxFormComponents.jsx
+++ src/ui/components/ReduxFormComponents.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import TextField from 'material-ui/TextField'
 
-export const ReduxTextField = ({input, meta, ...custom}: any) => {
+export const ReduxTextField = ({input, meta, ...custom}) => {
   return (
     <TextField
       error={meta && meta.touched && meta.error}
