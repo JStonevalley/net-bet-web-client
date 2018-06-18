d164569ad1ed84241059e45a43a48744b27fb9fd
Jonas Stendahl <jonas.stendahl@outlook.com>
2018-06-18 07:52:28 +0200

    Extract components

--- src/publicContent/components/GameSummaryCard.jsx
+++ src/publicContent/components/GameSummaryCard.jsx
@@ -10,6 +10,58 @@ import {DateTime} from 'luxon'
 import {BettingForm} from '../../betting/components/BettingForm'
 import {CurrentBetDisplay} from './CurrentBetDisplay'
 
+const TeamDisplay = ({name, crest, style}) => {
+  return <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    ...style
+  }}>
+    <Paper style={{borderRadius: '50%'}}>
+      <Avatar src={crest} style={{width: 56, height: 56}} />
+    </Paper>
+    <Typography variant='subheading' align='center' style={{flexBasis: '40%'}}>
+      {name}
+    </Typography>
+  </div>
+}
+
+const GameDateAndTimeDisplay = ({game, style}) => {
+  return <div style={style}>
+    <Typography variant='subheading' align='center' color='textSecondary' >
+      {game.date.setLocale('sv').toLocaleString(DateTime.DATE_MED)}
+    </Typography>,
+    <Typography variant='subheading' align='center' color='textSecondary' >
+      {game.date.setLocale('sv').toLocaleString(DateTime.TIME_24_SIMPLE)}
+    </Typography>
+  </div>
+}
+
+const GameResultDisplay = ({game}) => {
+  return <div>
+    {game.result.halfTime && <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+      <Typography variant='subheading' align='center' color='textSecondary' >
+        ({game.result.halfTime.get(game.homeTeam.id)})
+      </Typography>
+      <Typography variant='subheading' align='center' color='textSecondary' >
+        ({game.result.halfTime.get(game.awayTeam.id)})
+      </Typography>
+    </div>}
+    <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+      <Typography variant='title' align='center'>
+        {game.result.fullTime.get(game.homeTeam.id)}
+      </Typography>
+      <Typography variant='title' align='center'>
+        {game.result.fullTime.get(game.awayTeam.id)}
+      </Typography>
+    </div>
+  </div>
+}
+
+const teamDisplayStyle = {
+  flex: '0 1 6rem'
+}
+
 const GameSummaryCardPresentation = ({
   leagueId,
   game,
@@ -36,47 +88,21 @@ const GameSummaryCardPresentation = ({
     >
       <div style={sectionStyle}>
         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', flex: '0 1 6rem'}}>
-            <Paper style={{borderRadius: '50%'}}>
-              <Avatar src={game.homeTeam.crestUrl} style={{width: 56, height: 56}} />
-            </Paper>
-            <Typography variant='subheading' align='center' style={{flexBasis: '40%'}}>
-              {game.homeTeam.name}
-            </Typography>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', flex: '0 1 6rem'}}>
-            <Paper style={{borderRadius: '50%'}}>
-              <Avatar src={game.awayTeam.crestUrl} style={{width: 56, height: 56}} />
-            </Paper>
-            <Typography variant='subheading' align='center' style={{flexBasis: '40%'}}>
-              {game.awayTeam.name}
-            </Typography>
-          </div>
+          <TeamDisplay
+            name={game.homeTeam.name}
+            crest={game.homeTeam.crestUrl}
+            style={teamDisplayStyle}
+          />
+          <TeamDisplay
+            name={game.awayTeam.name}
+            crest={game.awayTeam.crestUrl}
+            style={teamDisplayStyle}
+          />
         </div>
       </div>
       <div style={sectionStyle}>
-        <Typography variant='subheading' align='center' color='textSecondary' >
-          {game.date.setLocale('sv').toLocaleString(DateTime.DATE_MED)}
-        </Typography>
-        <Typography variant='subheading' align='center' color='textSecondary' >
-          {game.date.setLocale('sv').toLocaleString(DateTime.TIME_24_SIMPLE)}
-        </Typography>
-        {game.result.halfTime && <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-          <Typography variant='subheading' align='center' color='textSecondary' >
-            ({game.result.halfTime.get(game.homeTeam.id)})
-          </Typography>
-          <Typography variant='subheading' align='center' color='textSecondary' >
-            ({game.result.halfTime.get(game.awayTeam.id)})
-          </Typography>
-        </div>}
-        <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-          <Typography variant='title' align='center'>
-            {game.result.fullTime.get(game.homeTeam.id)}
-          </Typography>
-          <Typography variant='title' align='center'>
-            {game.result.fullTime.get(game.awayTeam.id)}
-          </Typography>
-        </div>
+        <GameDateAndTimeDisplay game={game} />
+        <GameResultDisplay game={game} />
         {!game.isPlayed() && <Divider style={{margin: '1rem 0'}} />}
         {placeBetOpen
           ? <BettingForm
